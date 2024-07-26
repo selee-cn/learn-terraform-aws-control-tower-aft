51100afca1d596a2a6dc1e7bb02e600fc3f6d745
selee <se.lee@cloudnetworks.co.kr>
2024-07-26 13:17:43 +0900

    main

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ module "aws-control-tower-aft-main" {
   aft_management_account_id   = var.aft_management_account_id
   ct_home_region              = var.ct_home_region
   tf_backend_secondary_region = var.tf_backend_secondary_region
-
-  vcs_provider                                  = "github"
-  account_request_repo_name                     = "${var.github_username}/learn-terraform-aft-account-request"
-  account_provisioning_customizations_repo_name = "${var.github_username}/learn-terraform-aft-account-provisioning-customizations"
-  global_customizations_repo_name               = "${var.github_username}/learn-terraform-aft-global-customizations"
-  account_customizations_repo_name              = "${var.github_username}/learn-terraform-aft-account-customizations"
+  github_username             = var.github_username
+  #vcs_provider                                  = "github"
+  #account_request_repo_name                     = "${var.github_username}/learn-terraform-aft-account-request"
+  #account_provisioning_customizations_repo_name = "${var.github_username}/learn-terraform-aft-account-provisioning-customizations"
+  #global_customizations_repo_name               = "${var.github_username}/learn-terraform-aft-global-customizations"
+  #account_customizations_repo_name              = "${var.github_username}/learn-terraform-aft-account-customizations"
 }
